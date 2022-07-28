4d4eeb13d9e521d2257030584c1a0b94cf1a3a4f
AlexBF_Pro <alexfirmiano2@gmail.com>
2022-07-27 23:24:49 -0300

    Substituição img, tags da home e criação Style.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { FaArrowRight } from 'react-icons/fa'
+import { Container, Content, Infos, Name, Function, Intro, LinkProjects, Logo, Img } from '../../styles/indexStyle'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <h1>Teste</h1>
+   <Container>
+     <Content>
+      <Infos>
+        <Name>Olá, sou Alex Barbosa</Name>
+        <Function>Desenvolvedor FullStack / Administrador</Function>
+        <Intro>Técnico em Programação - Javascript e Typescript.  
+          Front-end e back-end (Full Stack) - Node, Express, noSQL, 
+          React, hooks, Redux, Design Patterns.
+          Além disto boas práticas como Manisfesto Ágil, Scrum, 
+          CICD-Pepiline, Git - Versionamento, Prettier, Lint.
+        </Intro>
+        <Link href="projects">
+          <LinkProjects>PROJETOS <FaArrowRight /></LinkProjects>
+        </Link>
+      </Infos>
+      <Logo>
+        <Img src="/images/perfil.jpg" alt="logo" />
+      </Logo>
+     </Content>
+   </Container>
   )
 }
